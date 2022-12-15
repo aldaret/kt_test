168724cf0ebe6685a4635f988e626aa4290d0ef2
/usr/bin/vim <support@beget.ru>
2022-12-15 16:01:58 +0300

    fix(weight): удалено лишнее

--- src/Services/WeightService.php
+++ src/Services/WeightService.php
@@ -9,11 +9,4 @@ class WeightService
         $arrayWeight = explode(' ', $weight);
         return ('kg' === $arrayWeight[1]) ? $arrayWeight[0] * 1000 : $arrayWeight[0];
     }
-
-    public function setIterator(array $array)
-    {
-        foreach ($array as $product) {
-            yield $product;
-        }
-    }
 }
